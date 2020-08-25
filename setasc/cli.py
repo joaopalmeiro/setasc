1c431af0f504f73ce658bf3e097d5a35ae97ef9f
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-25 23:51:07 +0100

    Updated: Utils and classifiers sorting.

--- setasc/cli.py
+++ setasc/cli.py
@@ -1,42 +1,13 @@
 import ast
-import shlex
 from parser import SetupVisitor
 from pathlib import Path
 
-from constants import CLASSIFIERS_LIST, SETUP
-from utils import convert_dict_to_single_str
-
-
-def remove_empty_strings(lst):
-    return [el for el in lst if el.strip()]
-
-
-def sort_classifiers(lst, ascending=True):
-    # This `key` is used to easily "ignore" quote marks within strings.
-    return sorted(lst, reverse=False if ascending else True, key=shlex.split)
-
-
-def get_indentation(el):
-    whitespaces = len(el) - len(el.lstrip())
-    ignoring_newline_whitespaces = len(el) - len(el.lstrip("\n"))
-
-    return whitespaces - ignoring_newline_whitespaces
-
-
-def get_classifiers_data(data):
-    classifiers_data = CLASSIFIERS_LIST.findall(data)
-    classifiers_data = classifiers_data[0].split(",")
-
-    return sort_classifiers(remove_empty_strings(classifiers_data))
-
-
-def join_classifiers_data(lst):
-    classifiers_str = ",".join(lst)
-
-    indentation = get_indentation(classifiers_str) // 2
-    end_whitespaces = " " * indentation
-
-    return f"classifiers=[{classifiers_str},\n{end_whitespaces}]"
+from constants import SETUP
+from utils import (
+    convert_dict_to_single_str,
+    convert_list_to_single_str,
+    sort_quoted_list,
+)
 
 
 def join_setup_arguments(dct):
@@ -50,6 +21,14 @@ def join_setup_arguments(dct):
         return f"setup({sorted_setup_arguments})"
 
 
+def sort_classifiers(string):
+    classifiers_list = string.lstrip("[").rstrip("]").split(", ")
+
+    sorted_classifers_list = sort_quoted_list(classifiers_list)
+
+    return convert_list_to_single_str(sorted_classifers_list)
+
+
 def main(file_path):
     try:
         path = Path(file_path)
@@ -59,18 +38,18 @@ def main(file_path):
     except IsADirectoryError:
         print(f"🚫 {repr(file_path)} is a directory, not a file.")
     else:
-        # classifiers_data = join_classifiers_data(get_classifiers_data(data))
-        # updated_data = CLASSIFIERS.sub(classifiers_data, data)
-
         root = ast.parse(data)
         setup_call = SetupVisitor()
         setup_call.visit(root)
+
+        setup_call.arguments[0]["classifiers"] = sort_classifiers(
+            setup_call.arguments[0]["classifiers"]
+        )
 
         sorted_setup_arguments = join_setup_arguments(setup_call.arguments[0])
         updated_data = SETUP.sub(sorted_setup_arguments, data)
 
         print(updated_data)
-
         # path.write_text(updated_data)
 
 
--- setasc/utils.py
+++ setasc/utils.py
@@ -1,3 +1,4 @@
+import shlex
 from collections import OrderedDict
 
 
@@ -12,3 +13,24 @@ def get_class_name(obj):
 
 def convert_dict_to_single_str(dct, item_sep=", ", pair_sep=" = "):
     return item_sep.join(f"{key}{pair_sep}{value}" for key, value in dct.items())
+
+
+def convert_list_to_single_str(lst):
+    return f"[{', '.join(lst)}]"
+
+
+def get_indentation(string):
+    whitespaces = len(string) - len(string.lstrip())
+    ignoring_newline_whitespaces = len(string) - len(string.lstrip("\n"))
+
+    return whitespaces - ignoring_newline_whitespaces
+
+
+def remove_empty_strings(lst):
+    # Strings that contain only the `\n` character or this character and some whitespaces are also removed.
+    return [el for el in lst if el.strip()]
+
+
+def sort_quoted_list(lst, ascending=True):
+    # This `key` is used to easily "ignore" quote marks within strings.
+    return sorted(lst, reverse=False if ascending else True, key=shlex.split)
